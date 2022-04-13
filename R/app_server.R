f130cc80ff08f813b990b3ac930dc7109e5ac909
RichardPilbery <email@pilbery.co.uk>
2022-04-13 16:35:58 +0100

    Prepping for external SimPy DES model to run.

--- R/app_server.R
+++ R/app_server.R
@@ -6,5 +6,12 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  # reticulate::use_condaenv("base")
+  # reticulate::py_run_file('D:/oneoneone/oneoneone-des.py')
+  # Can't get reticulate to place nicely with the SimPy installation in another folder
+  # Instead, we'll create a lookup ? CSV and then run the command below
+  # Once it has completed, we can retrieve the output
+  system("D:/conda/condabin/conda.bat activate base && D:/conda/condabin/conda.bat run D:/oneoneone/oneoneone-des.py", intern = T)
+
   mod_des_server("des_1")
 }
--- R/mod_des.R
+++ R/mod_des.R
@@ -10,7 +10,31 @@
 mod_des_ui <- function(id){
   ns <- NS(id)
   tagList(
-    h2('HELLO')
+    shinydashboard::dashboardPage(
+      shinydashboard::dashboardHeader(title="111 Demand modelling"),
+      shinydashboard::dashboardSidebar(
+        h2('Configuration', class = "sidebar-h2"),
+        sliderInput("num_runs", "Number of simualtion runs", 1, 10, 3, 1, TRUE),
+        sliderInput("warm_up_time", "Warm up time (hours)", 8, 24, 24, 1, TRUE),
+        sliderInput("pt_time_in_sim", "Patient time in simulation (hours)", 24, 96, 96, 2, TRUE),
+        collapsed = FALSE
+      ),
+      shinydashboard::dashboardBody(
+        # infoBoxes
+        fluidRow(
+          shinydashboard::infoBox(
+            "Orders", uiOutput("orderNum2"), "Subtitle", icon = icon("credit-card")
+          ),
+          shinydashboard::infoBox(
+            "Approval Rating", "60%", icon = icon("chart-line"), color = "green",
+            fill = TRUE
+          ),
+          shinydashboard::infoBox(
+            "Progress", uiOutput("progress2"), icon = icon("users"), color = "purple"
+          )
+        ),
+      )
+    )
   )
 }
 
